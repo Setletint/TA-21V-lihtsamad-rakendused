99d46a21135a3974597a1a1ebc2e988c85754a7d
Setletint <polikq772@gmail.com>
2022-03-24 11:06:50 +0200

    Creating + upd

--- Projects/TicTacToe.py
+++ Projects/TicTacToe.py
@@ -1,3 +1,56 @@
-print(". . x")
-print(". x .")
-print(". . o")
+print(". . .")
+print(". . .")
+print(". . .")
+
+next_pos_upd = input("Type here you want to set your figure: ")
+if(next_pos_upd == "1"):
+    print("x . .")
+    print(". . .")
+    print(". . .")
+    next_wgt_upd = input("Type which row: ")
+    if(next_wgt_upd == "1"):
+        print("x . .")
+        print(". . .")
+        print(". . .")
+    if(next_wgt_upd == "2"):
+        print(". . .")
+        print("x . .")
+        print(". . .")
+    if(next_wgt_upd == "3"):
+        print(". . .")
+        print(". . .")
+        print("x . .")
+elif(next_pos_upd == "2"):
+    print(". x .")
+    print(". . .")
+    print(". . .")
+    next_wgt_upd = input("Type which row: ")
+    if(next_wgt_upd == "1"):
+        print(". x .")
+        print(". . .")
+        print(". . .")
+    if(next_wgt_upd == "2"):
+        print(". . .")
+        print(". x .")
+        print(". . .")
+    if(next_wgt_upd == "3"):
+        print(". . .")
+        print(". . .")
+        print(". x .")
+elif(next_pos_upd == "3"):
+    print(". . x")
+    print(". . .")
+    print(". . .")
+    next_wgt_upd = input("Type which row: ")
+    if(next_wgt_upd == "1"):
+        print(". . x")
+        print(". . .")
+        print(". . .")
+    if(next_wgt_upd == "2"):
+        print(". . .")
+        print(". . x")
+        print(". . .")
+    if(next_wgt_upd == "3"):
+        print(". . .")
+        print(". . .")
+        print(". . x")
