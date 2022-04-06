d421e0e4d4ece192e67012d5626b88b3a47b1cba
Setletint <polikq772@gmail.com>
2022-04-06 14:29:44 +0300

    Update Lesson-2-1.py

--- Projects/Lesson-2-1.py
+++ Projects/Lesson-2-1.py
@@ -5,7 +5,7 @@ map = [
     [12, 0, 1, 0, 1],
     [1, 1, 1, 0, 1],
     [0, 0, 1, 1, 0],
-    [24, 0, 1, 0, 0],
+    [1, 0, 1, 0, 0],
     [1, 1, 1, 1, 1]
 ]
 
@@ -15,27 +15,36 @@ finish_pos = 0
 
 y_rows = 5
 x_rows = 5
+
+
 def get_next_pos(current_pos_y, current_pos_x):
+    if current_pos_y == 3 and current_pos_x == 0:
+        print("End")
+        quit()
     random_number = random.random()
+    print (random_number)
 
     can_go_right = current_pos_x+1<x_rows and map[current_pos_y][current_pos_x+1] == 1
-    can_go_left = current_pos_x-1 > 0 and map[current_pos_y][current_pos_x-1] == 1
-    can_go_top = current_pos_y-1 > 0 and map[current_pos_y-1][current_pos_x] == 1
+    can_go_left = current_pos_x-1 >= 0 and map[current_pos_y][current_pos_x-1] == 1
+    can_go_top = current_pos_y-1 >= 0 and map[current_pos_y-1][current_pos_x] == 1
     can_go_down = current_pos_y+1 < y_rows and map[current_pos_y+1][current_pos_x] == 1
-
-
-    if can_go_left:
-        print("Can go left")
-        return[current_pos_y, current_pos_x-1]
-    if can_go_right:
-        print("Can go right")
-        return [current_pos_y, current_pos_x+1]
-    if can_go_top:
-        print("Can go top")
-        return[current_pos_y-1 , current_pos_x ]
-    if current_pos_y+1 < y_rows and map[current_pos_y+1][current_pos_x] == 1:
-        print("You can go bottom") 
-        return[current_pos_y+1 , current_pos_x ]
+    while current_pos_x == current_pos_x and current_pos_y == current_pos_y:
+        random_number = random.random()
+        if can_go_left and random_number > 0.25 and random_number < 0.50:
+            print("Can go left")
+            return[current_pos_y, current_pos_x-1]
+        elif can_go_right and random_number > 0.50 and random_number < 0.75:
+            print("Can go right")
+            return [current_pos_y, current_pos_x+1]
+        elif can_go_top and random_number <0.25:
+            print("Can go top")
+            return[current_pos_y-1 , current_pos_x ]
+        elif can_go_down and random_number >0.75:
+            print("Can go down")
+            return[current_pos_y + 1, current_pos_x]
+    #if current_pos_y+1 < y_rows and map[current_pos_y+1][current_pos_x] == 1:
+    #    print("You can go bottom") 
+    #    return[current_pos_y+1 , current_pos_x ]
     #if current_pos_y-1 > 0 and map[current_pos_y-1][current_pos_x] == 1:
     #    print("You can go up") 
     #    return[current_pos_y-1 , current_pos_x ]
